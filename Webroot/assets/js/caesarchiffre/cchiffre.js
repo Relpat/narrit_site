ea38f5b5bc581f97fd1ec8a65332cc5c436a94b5
Relpat <info@relpat.de>
2019-04-30 17:30:44 +0200

    Add new tags (sup, underline )

--- Webroot/assets/js/caesarchiffre/cchiffre.js
+++ Webroot/assets/js/caesarchiffre/cchiffre.js
@@ -28,22 +28,16 @@ function CaeserChiffre() {
 
     function _setCurrentText(readText) {
         currentText = readText;
-        buggy.enableDebugTraceOnce();
-        buggy.message(arguments, "currentText", currentText);
     }
 
     function _parseText() {
         _readTextField();
         let currentText = _getCurrentText();
         let decodedText = Manipulator.htmlentities.decode(currentText);
-        buggy.message(arguments, "currentText", currentText);
-        buggy.message(arguments, "decodedText", decodedText);
-        buggy.message(arguments, "currentTextField", currentTextField);
         currentTextField.innerHTML = decodedText;
     };
 
     function _init(config) {
-        buggy.message(arguments, "Start CaesarChiffre with config", config);
         editForm = config.editForm;
 
         // make html-content editable
@@ -56,7 +50,6 @@ function CaeserChiffre() {
             let editFields = document.querySelectorAll(".cchiff-editable");
             for (let element of editFields) {
                 element.addEventListener("click", function (index, data) {
-                    buggy.message(arguments, "change currentfield")
                     currentTextField = this;
                     _readTextField();
                 });
@@ -65,20 +58,20 @@ function CaeserChiffre() {
 
         function createInteractiveMenu() {
             let mainMenu = $("<div/>", {
-                "class": "btn-group-vertical position-fixed float-right",
+                "class": "position-fixed float-right",
                 "role": "group"
             }).css({
                 "top": "120px",
                 "right": "60px"
             })
             createElementButtons();
+            // createHeadlines();
             $(editForm).append(mainMenu);
 
             /**
              * create the Buttons
              */
             function createElementButtons() {
-                buggy.message(arguments, "that", that);
                 let buttonsConfig = {
                     "bold": {
                         config: {
@@ -104,16 +97,103 @@ function CaeserChiffre() {
                             })
                         }
                     },
+                    "unterline": {
+                        config: {
+                            "class": "btn btn-primary text-action-button",
+                            "data-action-type": "u",
+                            "text": "Underline",
+                        },
+                        action: {
+                            "click": (function () {
+                                Manipulator.selectedText.surroundWithTag("u")
+                            })
+                        }
+                    },
+                    "sup": {
+                        config: {
+                            "class": "btn btn-primary text-action-button",
+                            "data-action-type": "u",
+                            "text": "Hochstellen",
+                        },
+                        action: {
+                            "click": (function () {
+                                Manipulator.selectedText.surroundWithTag("sup")
+                            })
+                        }
+                    },
                 };
 
+                let buttonGroup = $("<div/>", {
+                    "class": "btn-group-vertical float-right button-group-font-style",
+                    "role": "group"
+                });
                 /**
                  * add buttons
                  */
                 for (let buttontype in buttonsConfig) {
                     let button = $("<button/>", buttonsConfig[buttontype].config);
                     $(button).on(buttonsConfig[buttontype].action);
-                    mainMenu.append(button);
+                    buttonGroup.append(button);
                 }
+
+                mainMenu.append(buttonGroup);
+            }
+
+            function createHeadlines(){
+                let texttypeConfig = {
+                "h1": {
+                    config: {
+                        "class": "btn btn-primary text-action-button",
+                            "data-action-type": "h1",
+                            "text": "h1",
+                    },
+                    action: {
+                        "click": (function () {
+                            Manipulator.selectedText.surroundWithTag("h1")
+                        })
+                    }
+                },
+                "h2": {
+                    config: {
+                        "class": "btn btn-primary text-action-button",
+                            "data-action-type": "h2",
+                            "text": "h2",
+                    },
+                    action: {
+                        "click": (function () {
+                            Manipulator.selectedText.surroundWithTag("h2")
+                        })
+                    }
+                },
+                "h3": {
+                    config: {
+                        "class": "btn btn-primary text-action-button",
+                            "data-action-type": "h3",
+                            "text": "h3",
+                    },
+                    action: {
+                        "click": (function () {
+                            Manipulator.selectedText.surroundWithTag("h3")
+                        })
+                    }
+                    }
+                };
+
+
+                let buttonGroup = $("<div/>", {
+                    "class": "btn-group-vertical float-right button-group-headlines",
+                    "role": "group"
+                });
+
+                /**
+                 * add buttons
+                 */
+                for (let buttontype in texttypeConfig) {
+                    let button = $("<button/>", texttypeConfig[buttontype].config);
+                    $(button).on(texttypeConfig[buttontype].action);
+                    buttonGroup.append(button);
+                }
+                mainMenu.append(buttonGroup);
             }
         }
     }
@@ -121,11 +201,9 @@ function CaeserChiffre() {
     let Manipulator = new _Manipulator();
 
     function _Manipulator() {
-        buggy.message(arguments, "init Manipulator");
         let that = this;
         return {
             htmlentities: function () {
-                buggy.message(arguments, "init htmlentities");
 
                 /**
                  * Converts a string to its html characters completely.
@@ -168,7 +246,6 @@ function CaeserChiffre() {
                 }
             }(),
             selectedText: function () {
-                buggy.message(arguments, "init selectedText");
 
                 /**
                  * Returns the selected string in the editable-content-area
@@ -179,7 +256,7 @@ function CaeserChiffre() {
                     let isEditableContent = false;
                     let selection = window.getSelection();
 
-                    if (currentTextField === selection.anchorNode.parentElement) {
+                    if (currentTextField.contains(selection.anchorNode.parentElement) || currentTextField === selection.anchorNode.parentElement) {
                         isEditableContent = true;
                     }
                     if (isEditableContent) {
@@ -197,14 +274,35 @@ function CaeserChiffre() {
                  * @param tag
                  */
                 function _surroundWithTag(tag) {
-                    if (_getSelection() === false) {
+                    if (typeof _getSelection() !== "object") {
                         return;
                     }
                     let selectedText = _getSelection().toString();
-                    let surroundedText = "&lt;" + tag + "&gt;" + selectedText + "&lt;/" + tag + "&gt;";
+                    let clearedText = findTagAndRemoveIt(selectedText,tag);
+                    let surroundedText = "&lt;" + tag + "&gt;" + clearedText + "&lt;/" + tag + "&gt;";
+
+                    buggy.message(arguments, "selectedText", selectedText);
+                    buggy.message(arguments, "clearedText", clearedText);
+                    buggy.message(arguments, "surroundedText", surroundedText);
 
                     replaceSelectedText(surroundedText);
                     _parseText();
+
+                    /**
+                     * removes the tag in the selected replacement string
+                     * @param replacementText
+                     * @param tag
+                     */
+                    function findTagAndRemoveIt(replacementText, tag) {
+                        let decoded = Manipulator.htmlentities.decode(replacementText);
+
+                        let tagBegin = new RegExp("&lt;" + tag + "&gt;", "g");
+                        let tagEnd = new RegExp("&lt;" + tag + "&gt;", "g");
+
+                        return String(decoded)
+                            .replace(tagBegin, '')
+                            .replace(tagEnd, '');
+                    }
 
                     /**
                      * source: https://stackoverflow.com/questions/3997659/replace-selected-text-in-contenteditable-div
@@ -226,13 +324,13 @@ function CaeserChiffre() {
                     }
                 }
 
+
                 /**
                  * Return namespace-declaration
                  */
                 return {
                     getSelection: _getSelection,
                     surroundWithTag: _surroundWithTag
-
                 }
             }()
         }
